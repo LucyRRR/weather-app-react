c40b52dff4fe2f5df12f09cd11cb7ea347005291
LucyRRR <lucka.roth@gmail.com>
2021-06-10 15:14:12 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./index.css";
 import WeatherForm from "./WeatherForm.js";
 import './App.css';
 import Link from "./Link";
@@ -6,9 +6,11 @@ import Link from "./Link";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <WeatherForm />
       <Link />
+      </div>
     </div>
   );
 }
