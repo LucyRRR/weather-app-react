3c8939a8f90e60fe7acbfe3e4894c04e0bd942cb
LucyRRR <lucka.roth@gmail.com>
2021-06-10 14:18:52 +0200

    added link to my github

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import WeatherForm from "./WeatherForm.js";
 import './App.css';
+import Link from "./Link";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <WeatherForm />
+      <Link />
     </div>
   );
 }
---
+++ src/Link.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Link () {
+    return (
+        <div>
+            <br/>
+        Coded by Lucia Rothova stored on
+        <a href="https://github.com/LucyRRR/weather-app-react"
+        target="_blank" rel="noopener noreferrer"> GitHub</a>
+        </div>
+    )
+}
