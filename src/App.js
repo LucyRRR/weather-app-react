fbd21a2929e0c53166108df30d4fad1f09a38a2f
LucyRRR <lucka.roth@gmail.com>
2021-06-11 14:28:36 +0200

    deleted link component, imported css to relevant components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./index.css";
 import WeatherForm from "./WeatherForm.js";
-import './App.css';
-import Link from "./Link";
+import './Weather.css';
+
 
 export default function App() {
   return (
@@ -9,7 +9,12 @@ export default function App() {
       <div className="container">
       <h1>Weather App</h1>
       <WeatherForm />
-      <Link />
+      <div className="link-code">
+            <br/>
+        Coded by <b>Lucia Rothova</b> and is open-sourced on
+        <a className="Link" href="https://github.com/LucyRRR/weather-app-react"
+        target="_blank" rel="noopener noreferrer"> GitHub</a>
+        </div>
       </div>
     </div>
   );
