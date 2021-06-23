329666500ebaadd4bd55076767812963cec7e4ac
LucyRRR <lucka.roth@gmail.com>
2021-06-23 17:28:31 +0200

    inserted loop with more forecast days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,9 +18,17 @@ export default function WeatherForecast(props) {
         return (
         <div className="WeatherForecast">
             <div className="row">
-                <div className="col">
-                    <WeatherforecastDay data={forecast[0]} />
+                {forecast.map(function(dailyForecast, index) {
+                    if (index < 5) {
+
+                return (
+                    <div className="col" key={index}>
+                    <WeatherforecastDay data={dailyForecast} />
                     </div>
+                );
+                }
+                })}
+                
                 </div>
             </div>   
     );
