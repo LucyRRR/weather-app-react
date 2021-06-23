326b3ee6997373e90d127a994f299745578d3c6e
LucyRRR <lucka.roth@gmail.com>
2021-06-23 17:14:08 +0200

    inserted forecast for 1 day + date

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,37 +1,41 @@
-import React from "react";
+import React, { useState} from "react";
 import WeatherIcon from "./WeatherIcon.js";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherforecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+    
     function handleResponse(response) {
-        console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded(true); 
     }  
 
+    if (loaded) {
+        
+        return (
+        <div className="WeatherForecast">
+            <div className="row">
+                <div className="col">
+                    <WeatherforecastDay data={forecast[0]} />
+                    </div>
+                </div>
+            </div>   
+    );
+
+} else {
     let apiKey = "9561e0b8516730d7561152d7deb2d27b";
     let longitude = props.coord.lon;
     let latitude = props.coord.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
+    
+        return null;
+}
+}
+    
 
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                    <div className="WeatherForecas-day"> Thu    
-                    </div> 
-                    <WeatherIcon code="02n" size={36}/>
-                        <div className="Weatherforecast-temps">
-                            <span className="WeatherForecast-temps-max">
-                    24°
-                            </span>
-                            <span className="WeatherForecast-temps-min">
-                    17°
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>   
-    )
-};
+    
